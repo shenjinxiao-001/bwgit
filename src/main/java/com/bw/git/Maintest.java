920aca0a0d7311e849c08ba1e439160740964737
lianxi2 <1694777256@qq.com>
2020-03-20 00:04:35 +0800

    修改完成！！！

--- src/main/java/com/bw/git/Maintest.java
+++ src/main/java/com/bw/git/Maintest.java
@@ -2,6 +2,9 @@ package com.bw.git;
 
 public class Maintest {
     public static void main(String[] args) {
+
         System.out.println("武汉加油，中国加油！！！");
+        System.out.println("北京加油，山西加油！！！");
+
     }
 }
